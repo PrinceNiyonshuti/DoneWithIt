fbbf978be55d58f7379bd51a69247ad28c51ae06
PrinceNiyonshuti <npprince47@gmail.com>
2021-02-18 19:38:01 +0200

    testing one line

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,SafeAreaView,ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Testing app not connected </Text>
+      <Image source={{
+        width:200,
+        height:300,
+        uri:"https://picsum.photos/200/300",
+      }}/>
+
+      <Text>test data now</Text>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    alignItems:"center",
+    justifyContent:"center",
   },
-});
+}); 
